9f221d9a30f540abc2d84bce70f76aa814fdf88a
Jacastro619 <jorgecastro619@gmail.com>
2023-09-26 16:21:24 -0700

    add Psuedocode to bottom of script file

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Psuedocode
+// Declare generatePassword function
+// Prompt user with a question of how many characters are desired in password
+// If answer is under 8 character and over 128 characters then its an invalid response
+// Confirm whether user wants uppercase letters in password
+// Confirm whether user wants lowercase letters in password
+// Confirm whether user wants numbers in password
+// Confirm whether user wants special characters in password
+// If user does not choose "ok" on all, alert "must choose at least one"
+// Store responses in variables
+// Store uppercase letters in a variable
+// Store lowercase letters in a variable
+// Store numbers in a variable
+// Store special characters in a variable
+// Declare an empty array
+// Combined predefined values that can be in password in a string
+// Create a for loop with character amount as the number of iterations the loop will execute
+// In the loop push the string[random index] into empty array
+// Once loop is completed create a string from the array
+// Return password string
+
+
+
+
+console.log(Math.floor(Math.random() * 10/*charactersValue*/))
